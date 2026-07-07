b951c9c7a8195c4fc8e17850dd4d1e7d377ea0b9
DevKpi <thiagobelpo@gmail.com>
2026-07-07 17:03:27 -0300

    Refactor: encapsular logica de inicio de sesion y progreso en el modelo Usuario

--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-class User {
+class Usuario {
     #password
     constructor(id, email, pass) {
         this.id = id;
@@ -7,18 +7,35 @@ class User {
         this.progress = 0;
     }
 
-    AnotarResultados(){
-
-    }
-
-    LogIn(){
-
-    }
-
-    LogOut(){
+    AnotarResultados(partidos){
+        const totalMatches = 104;
+        const finishedMatches = partidos.filter(m => m.finished === 'TRUE' || m.finished === true || m.finished === 'true').length;
+        this.progress = Math.round((finishedMatches / totalMatches) * 100);
         
+        return {
+            finishedMatches,
+            totalMatches,
+            percentage: this.progress
+        };
     }
 
+    static LogIn(username){
+        if (!username) return false;
+        localStorage.setItem('worldcup2026_user', username);
+        return true;
+    }
+
+    static LogOut(){
+        localStorage.removeItem('worldcup2026_user');
+    }
+
+    static ObtenerUsuarioActual() {
+        const username = localStorage.getItem('worldcup2026_user');
+        if (username) {
+            return new Usuario(username, username, null);
+        }
+        return null;
+    }
 }
 
 export default Usuario;
